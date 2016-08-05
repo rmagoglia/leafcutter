2a107307a93080d0efebaceaed14c55352ed0560
petercombs <pcombs@stanford.edu>
2016-08-05 15:30:54 -0700

    Rewrite major bits in python to use pysam
    The use of perl and no pysam made parsing the cigar strings very
    fragile, at best.  I believe this also should use less CPU, and make the
    calls more flexible.   Pritchard lab

--- scripts/bam2junc.sh
+++ scripts/bam2junc.sh
@@ -1,8 +1,9 @@
 
 leafCutterDir=$(dirname $0)/../
+echo $leafCutterDir
 bamfile=$1
 bedfile=$1.bed
 juncfile=$2
-samtools view $bamfile | python $leafCutterDir/scripts/filter_cs.py | $leafCutterDir/scripts/sam2bed.pl --use-RNA-strand - $bedfile
+python $leafCutterDir/scripts/filter_cs.py $bamfile | python $leafCutterDir/scripts/sam2bed.py --use-RNA-strand - $bedfile
 $leafCutterDir/scripts/bed2junc.pl $bedfile $juncfile
 rm $bedfile
--- scripts/filter_cs.py
+++ scripts/filter_cs.py
@@ -1,28 +1,30 @@
 import sys
+from pysam import AlignmentFile
+from argparse import ArgumentParser
 
 valid_spliced_reads=0
 problem_reads=0
 
-for ln in sys.stdin:
-    if ln[0] == "@": continue
-    ln_split=ln.split()
-    cs = ln_split[5]
-    if "N" in cs:
-        quality_score=int(ln_split[4])
-        if quality_score < 10: continue # quality score
-                
-        try:
-            L = int(cs.split("N")[0].split("M")[-1])
-            cs_split_M=cs.split("M")
-            edge5, edge3 = cs_split_M[0], cs_split_M[-2].split("N")[-1]
-            minedge = min([int(edge5), int(edge3)])
-        except ValueError:
-            problem_reads += 1
-            continue
-            
-        if L > 50 and minedge >= 6: # intron length must be > 50 and 6nt must map into each exon
-            valid_spliced_reads += 1
-            if valid_spliced_reads % 100000 == 0: sys.stderr.write("%d valid, %d problematic spliced reads\n" % (valid_spliced_reads, problem_reads) )
-            sys.stdout.write(ln)
-        elif minedge < 6:
-            pass
+parser = ArgumentParser()
+parser.add_argument('infile', nargs='?', default='-')
+parser.add_argument('outfile', nargs='?', default='-')
+args = parser.parse_args()
+infile = AlignmentFile(args.infile, 'r')
+outfile = AlignmentFile(args.outfile, 'wh', template=infile)
+
+for read in infile:
+    splice_len = 0
+    min_edge = 1e6
+    if read.mapping_quality < 10: continue
+    for cig_op, cig_len in read.cigartuples:
+        if cig_op == 3: # N
+            splice_len += cig_len
+        elif cig_op == 0:
+            min_edge = min(min_edge, cig_len)
+    if splice_len > 50 and min_edge >= 6:
+        outfile.write(read)
+        valid_spliced_reads += 1
+        if valid_spliced_reads % 100000 == 0:
+            sys.stderr.write("%d valid, %d problematic spliced reads\n" % (valid_spliced_reads, problem_reads) )
+sys.stderr.write("%d valid, %d problematic spliced reads\n" % (valid_spliced_reads, problem_reads) )
+
